49f1b04b46e2104a1a21944639331af7c5b16254
Harsha107 <harshadeep107@gmail.com>
2025-02-13 10:40:10 +0400

    code optimization

--- app/user-profile/components/UserProfileSettings.tsx
+++ app/user-profile/components/UserProfileSettings.tsx
@@ -1,45 +1,46 @@
 import React from "react";
 import { FaKey, FaUser, FaTrash } from "react-icons/fa";
+import './styles.css';
 
 export default function UserProfileSettings() {
     return (
-        <div className="bg-transparent mt-16 ml-0 2xl:ml-64"> 
-            <main className="flex flex-col flex-1 gap-4 mx-4">
-                    <div className="flex flex-col gap-1 bg-white shadow-md rounded-lg p-6 mt-4">
+        <div className="bg-transparent mb-4"> 
+            <main className="flex flex-col flex-1 gap-4">
+                    <section className="flex flex-col gap-1 bg-white shadow-md rounded-lg p-6 mt-4">
                         <h1 className="font-bold text-lg">Settings</h1>
                         <p className="text-gray-600 text-sm">Manage your account preferences and system settings</p>
-                    </div>
+                    </section>
 
-                    <div className="flex flex-row gap-4">
+                    <section className="flex flex-row gap-4">
                         <div className="flex flex-col bg-white shadow-md rounded-lg w-2/3 p-4 gap-4">
-                            <h1 className="font-bold text-xm">General Settings</h1>
+                            <h1 className="font-bold text-sm">General Settings</h1>
 
-                            <div className="flex flex-col gap-2">
-                                <h2 className="font-semibold">Email Notifications</h2>
-                                <div className="flex flex-row justify-between items-center">
-                                    <span className="flex flex-col">
+                            <div className="ups-div">
+                                <h2 className="ups-h2">Email Notifications</h2>
+                                <div className="ups-subdiv">
+                                    <span className="ups-span">
                                         <h3>New User Registration</h3>
-                                        <p className="font-extralight text-xm">Get notified when a user registers</p>
+                                        <p className="ups-p">Get notified when a user registers</p>
                                     </span>
-                                    <label className="inline-flex items-center cursor-pointer">
+                                    <label className="ups-label">
                                         <input type="checkbox" value="" className="sr-only peer"/>
                                         <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
                                     </label>
                                 </div>
-                                <div className="flex flex-row justify-between items-center">
-                                    <span className="flex flex-col">
+                                <div className="ups-subdiv">
+                                    <span className="ups-span">
                                         <h3>New Post Alerts</h3>
-                                        <p className="font-extralight text-xm">Recieve alerts for new posts</p>
+                                        <p className="ups-p">Recieve alerts for new posts</p>
                                     </span>
-                                    <label className="inline-flex items-center cursor-pointer">
+                                    <label className="ups-label">
                                         <input type="checkbox" value="" className="sr-only peer"/>
                                         <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
                                     </label>
                                 </div>
                             </div>
 
-                            <div className="flex flex-col gap-2">
-                                <h2 className="font-semibold">Data Display</h2>
+                            <div className="ups-div">
+                                <h2 className="ups-h2">Data Display</h2>
                                 <div>
                                     <h3>Default Range</h3>
                                     <select name="" id="">
@@ -56,14 +57,14 @@ export default function UserProfileSettings() {
                                 </div>
                             </div>
 
-                            <div className="flex flex-col gap-2">
-                                <h2 className="font-semibold">Security</h2>
-                                <div className="flex flex-row justify-between items-center">
-                                    <span className="flex flex-col">
+                            <div className="ups-div">
+                                <h2 className="ups-h2">Security</h2>
+                                <div className="ups-subdiv">
+                                    <span className="ups-span">
                                         <h3>Two-Factor Authentication</h3>
-                                        <p className="font-extralight text-xm">Add an extra layer of security</p>
+                                        <p className="ups-p">Add an extra layer of security</p>
                                     </span>
-                                    <label className="inline-flex items-center cursor-pointer">
+                                    <label className="ups-label">
                                         <input type="checkbox" value="" className="sr-only peer"/>
                                         <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
                                     </label>
@@ -73,42 +74,42 @@ export default function UserProfileSettings() {
 
                         <div className="bg-white shadow-md rounded-lg flex flex-col justify-between w-1/3 p-4">
                             <div>
-                                <h1>Quick Actions</h1>
-                                <ul>
-                                    <li>
+                                <h1 className="font-bold mb-6">Quick Actions</h1>
+                                <ul className="flex flex-col gap-4">
+                                    <li className="ups-li1">
                                         <FaUser />
-                                        <span>Update Profile</span>
+                                        <span className="ups-span2">Update Profile</span>
                                     </li>
-                                    <li>
+                                    <li className="ups-li1">
                                         <FaKey />
-                                        <span>Change Password</span>
+                                        <span className="ups-span2">Change Password</span>
                                     </li>
-                                    <li>
+                                    <li className="ups-li1">
                                         <FaTrash />
-                                        <span>Delete Account</span>
+                                        <span className="ups-span2">Delete Account</span>
                                     </li>
                                 </ul>
                             </div>
 
                             <div>
-                                <h1>System Information</h1>
-                                <ul>
-                                    <li>
-                                        <span>Version:</span>
-                                        <span>2.1.0</span>
+                                <h1 className="font-bold mb-6">System Information</h1>
+                                <ul className="ups-div">
+                                    <li className="ups-li2">
+                                        <span className="ups-span2">Version:</span>
+                                        <span className="ups-span3">2.1.0</span>
                                     </li>
-                                    <li>
-                                        <span>Last Updated:</span>
-                                        <span>Jan 15, 2024</span>
+                                    <li className="ups-li2">
+                                        <span className="ups-span2">Last Updated:</span>
+                                        <span className="ups-span3">Jan 15, 2024</span>
                                     </li>
-                                    <li>
-                                        <span>Storage Used:</span>
-                                        <span>1.2 GB / 1.5 GB</span>
+                                    <li className="ups-li2">
+                                        <span className="ups-span2">Storage Used:</span>
+                                        <span className="ups-span3">1.2 GB / 1.5 GB</span>
                                     </li>
                                 </ul>
                             </div>
                         </div>
-                    </div>
+                    </section>
             </main>
         </div>
     )
--- app/user-profile/page.tsx
+++ app/user-profile/page.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Image from "next/image";
 import pfp from '../../public/pfp-img.jpg';
 import Link from "next/link";
+import UserProfileSettings from "./components/UserProfileSettings";
 
 export default function UserProfile() {
     return (
         <div className="bg-transparent mt-16 ml-0 2xl:ml-64"> 
             <main className="flex flex-col flex-1 gap-4 pt-4 mx-4">
-                <div className="bg-white flex flex-row items-center justify-between p-6 shadow-md rounded-lg">
+                <section className="bg-white flex flex-row items-center justify-between p-6 shadow-md rounded-lg">
                     <div className="flex flex-row justify-center gap-4 items-center">
                         <Image src={pfp} alt="user-pfp" className="w-20 h-20 rounded-full" />
                         <div className="flex flex-col gap-1">
@@ -17,13 +18,12 @@ export default function UserProfile() {
                         </div>
                     </div>
                     <div className="flex flex-row gap-6">
-                        <Link href='/user-profile/settings' className="bg-white text-blue-500 rounded-lg border border-blue-500 p-2 hover:bg-blue-500 hover:text-white">Settings</Link>
                         <button className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black hover:border hover:border-black">Edit Profile</button>
                         <button className="bg-white text-red-500 rounded-lg border border-red-500 p-2 hover:bg-red-500 hover:text-white">Deactivate Account</button>
                     </div>
-                </div>
+                </section>
 
-                <div className="bg-transparent flex flex-col lg:flex-row justify-between gap-4">
+                <section className="bg-transparent flex flex-col lg:flex-row justify-between gap-4">
                     <div className="bg-white p-4 shadow-md flex flex-col gap-3 flex-1 rounded-lg">
                         <span className="font-bold text-lg ml-3">Personal Information</span>
                         <span className="ml-3">
@@ -83,9 +83,9 @@ export default function UserProfile() {
                             <p className="font-extralight text-sm ml-7">3 days ago</p>    
                         </span>
                     </div>
-                </div> 
+                </section> 
 
-                <div className="bg-white p-4 shadow-md flex flex-col gap-3 rounded-lg">
+                <section className="bg-white p-4 shadow-md flex flex-col gap-3 rounded-lg">
                     <span className="font-bold text-lg ml-3">Recent Posts</span>
                     <span className="ml-3">
                         <p className="font-medium">Getting Started with Web Development</p>
@@ -102,7 +102,9 @@ export default function UserProfile() {
                         <p className="font-light">Deep dive into asynchronous programming...</p>
                         <p className="font-extralight">Posted 3 days ago - 12 mins ago</p>    
                     </span>  
-                </div>   
+                </section>   
+
+                <UserProfileSettings />
             </main>  
         </div>
     )
